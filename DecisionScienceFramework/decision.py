21b8344ee2e1b516f007b3dcee8433db08c20a93
Alex Perusse <perusse.a@gmail.com>
2018-03-22 16:29:28 -0700

    added eol and visualization of variables

--- DecisionScienceFramework/decision.py
+++ DecisionScienceFramework/decision.py
@@ -1,5 +1,7 @@
 # Core Python
 from collections import OrderedDict
+from itertools import product
+from math import ceil
 import pickle
 
 # Anaconda
@@ -90,7 +92,7 @@ class Decision(object):
         We split this out only so that we could norm on the loss column name.
         """
         self.add_variable_from_patsy('loss', formula_like)
-        self.last_run['isLoss'] = np.where(self.last_run.loss < 0, 1, 0)
+        self.last_run['isLoss'] = np.where(self.last_run.loss > 0, 1, 0)
 
     def sample(self, nsamples):
         with self.model:
@@ -108,6 +110,7 @@ class Decision(object):
             pass
 
     def plot_loss(self):
+        """Plot a histogram of the losses"""
         # Calculate a histogram in numpy 
         hist, edges = np.histogram(self.last_run.loss, bins=50, density=True)
         # Fix edges so that they are midpoints of the different slices instead
@@ -118,7 +121,7 @@ class Decision(object):
         loss_hist = pd.DataFrame(np.hstack([midpoints.astype('int'), 
                                             hist.reshape(-1, 1)]), 
                                  columns=["x", "y"])
-        loss_hist["isLoss"] = np.where(loss_hist.x < 0, 1, 0)
+        loss_hist["isLoss"] = np.where(loss_hist.x > 0, 1, 0)
         # Do the plotting
         fig, axs = plt.subplots(1, 1, figsize=(10, 10))
         sns.barplot("x", "y", hue="isLoss", data=loss_hist, ax=axs)
@@ -128,6 +131,29 @@ class Decision(object):
         axs.set_ylim(0, hist[-10:].mean())
         axs.legend(loc=1)
         plt.xticks(rotation=65)
+        plt.show()
+
+    def plot_variables(self):
+        """Plot distributions of all input variables"""
+        samples = self.last_run
+        cols = samples.columns
+        ncols = len(cols)
+        if ncols <= 8:
+            grid_cols = 2
+            grid_rows = ceil(ncols/2)       
+        else:
+            grid_cols = 3
+            grid_rows = ceil(ncols/3)
+        
+        fig, axs = plt.subplots(grid_rows, grid_cols, figsize=(30, 30))
+        for ix, coord in enumerate(product(range(0, grid_rows), range(0, grid_cols))):
+            try:
+                axs[coord].hist(samples[cols[ix]], bins=50)
+                axs[coord].set_xlabel(cols[ix])
+                #axs[coord].rc("axes", titlesize=20)
+            except:
+                pass
+        
         plt.show()
 
     def summary(self):
@@ -148,12 +174,28 @@ class Decision(object):
         Our information value 
         """
 
-        baseline_uncertainty = self.last_run['loss'].mean()
+        baseline_uncertainty = self.eol(self.last_run)
         mu = self.last_run[column].mean()
         fixed = self.last_run.copy()
         fixed[column] = mu
         for key, item in school_decision.deterministic.items():
             var = patsy.dmatrix(f"I({item}) - 1", data=fixed)
             fixed[key] = np.asarray(var)
-        updated_uncertainty = fixed['loss'].mean()
-        return updated_uncertainty - baseline_uncertainty
+        updated_uncertainty = self.eol(fixed)
+        return baseline_uncertainty - updated_uncertainty
+
+    def eol(self, samples):
+        """Calculate the Expected Opportunity Loss for a given set of samples
+        
+        First we consider the Expected Loss over our sample. This indicates what our "default" position should be. If 
+        it is negative (which corresponds to a positive outcome), then we decide to accepting the proposal.  Otherwise if the
+        expected loss is positive, then we default to declining the proposal.  Given that default, the Expected Opportunity
+        Loss is thus the average outcome if we had done the opposite.  So we sum up the losses where, if we accepted the proposal, 
+        then that declining the proposal would have actually been the right decision and divide that over the total number of
+        simulations considered.
+        """
+        exp_loss = samples.loss.mean()
+        if exp_loss < 0:
+            return sum(samples[samples.loss > 0].loss) / samples.shape[0]
+        else:
+            return sum(samples[samples.loss < 0].loss) / samples.shape[0]
